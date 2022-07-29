c05b3f998356003dc3a00cd3ac155624138b4b49
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-30 02:33:52 +0300

    Remove public validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,5 +6,4 @@ class Recipe < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
-  validates :public, presence: true
 end
