4dbed32f878af05e8aaa0305521444e0dc37bd4c
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-30 20:28:22 +0300

    Add tests for the models, views and requests

---
+++ spec/views/foods_index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Show Foods', type: :feature do
+  describe 'Food' do
+    before(:each) do
+      @user = User.create!(name: 'Cathy', email: 'cathy@gmail.com', password: 'mightyGod', password_confirmation: 'mightyGod')
+
+      @food = @user.foods.create!(name: 'Beef', measurement_unit: 'kg', price: 20, quantity: 1)
+
+      visit user_foods_path(@user)
+    end
+
+    it 'shows the food name' do
+      expect(page).to have_content('Beef')
+    end
+
+    it 'shows the measurement_unit' do
+      expect(page).to have_content('kg')
+    end
+
+    it 'shows the food price' do
+      expect(page).to have_content(20)
+    end
+
+    it 'shows the food quantity' do
+      expect(page).to have_content(1)
+    end
+  end
+end
